474a15d77832066d5fa9ad368d92c19fce627490
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2021-05-02 11:57:03 -0300

    Add postgres example

--- index.js
+++ index.js
@@ -2,8 +2,30 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World! Heroku !')
+})
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null };
+    res.send(results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
 })
 
 app.listen(PORT, () => {
