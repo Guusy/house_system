3d521a491d55e8162b928a6fa4c5d7e5a28fce1a
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2021-05-15 13:08:41 -0300

    Add react client POC

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const PORT = process.env.PORT || 5000
+const path = require('path');
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.static(buildPath));
+
 const todoItemsRoute = require('./routes/todo-items')
 
+app.use(cors());
 app.use(bodyParser.json());
-app.use('/todo-items',todoItemsRoute)
+app.use('/todo-items', todoItemsRoute)
 
 
 app.listen(PORT, () => {
